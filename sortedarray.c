0380d40c2e72e394afa6b0b40db0c5ce6fd17b92
Grace Mariya <gracemariya01@gmail.com>
2026-01-31 20:02:24 +0530

    Create sortedarray.c

---
+++ sortedarray.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <limits.h>
+
+double findMedianSortedArrays(int* nums1, int m, int* nums2, int n) {
+    // Ensure nums1 is the smaller array
+    if (m > n)
+        return findMedianSortedArrays(nums2, n, nums1, m);
+
+    int low = 0, high = m;
+
+    while (low <= high) {
+        int cut1 = (low + high) / 2;
+        int cut2 = (m + n + 1) / 2 - cut1;
+
+        int left1  = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+        int left2  = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+        int right1 = (cut1 == m) ? INT_MAX : nums1[cut1];
+        int right2 = (cut2 == n) ? INT_MAX : nums2[cut2];
+
+        if (left1 <= right2 && left2 <= right1) {
+            // Correct partition found
+            if ((m + n) % 2 == 0) {
+                int leftMax = left1 > left2 ? left1 : left2;
+                int rightMin = right1 < right2 ? right1 : right2;
+                return (leftMax + rightMin) / 2.0;
+            } else {
+                return (left1 > left2) ? left1 : left2;
+            }
+        } 
+        else if (left1 > right2) {
+            high = cut1 - 1;
+        } 
+        else {
+            low = cut1 + 1;
+        }
+    }
+    return 0.0; // will never reach here
+}
